bd0ae9e8e47b7922d0e965428b8ec4c5a477fa04
Igor <icholii@n-ix.com>
2017-09-22 15:17:55 +0300

    [add] logging interceptor added

--- app/src/main/java/com/choliy/igor/retrofitgithub/rest/ApiClient.java
+++ app/src/main/java/com/choliy/igor/retrofitgithub/rest/ApiClient.java
@@ -1,5 +1,9 @@
 package com.choliy.igor.retrofitgithub.rest;
 
+import com.choliy.igor.retrofitgithub.BuildConfig;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -13,8 +17,25 @@ public class ApiClient {
             sRetrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
+                    .client(addLoggingClient()) // (optional) for logging calls
                     .build();
         }
         return sRetrofit;
+    }
+
+    private static OkHttpClient addLoggingClient() {
+        // create Http client
+        OkHttpClient.Builder okHttpClientBuilder = new OkHttpClient.Builder();
+
+        // create logging interceptor
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+
+        // set type level of logs (how many info logs will show up)
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BASIC);
+
+        // add interceptor only in debug mode for better performance in release version
+        if (BuildConfig.DEBUG) okHttpClientBuilder.addInterceptor(loggingInterceptor);
+
+        return okHttpClientBuilder.build();
     }
 }
